6f9e376cda9877811b97bac4454c7325f3494633
pablocosta <pablo.botton.costa@gmail.com>
2023-10-23 13:15:28 -0300

    Add files via upload

---
+++ model.py
@@ -0,0 +1,4 @@
+https://aclanthology.org/2022.findings-naacl.47.pdf - esse data-set ESTÁ NO GITHUB e tem dados apra ingles, espanhol, alemao e outras línguas (para o português podemos usar o ola, bonjur, salve)
+
+chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://aclanthology.org/2022.iwslt-1.30.pdf - USAR AS MÉTRICAS DESSE ARTIGO PARA IDENTIFICAÇÃO DE FRASES COMPLEXAS
+
